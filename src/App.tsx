6dddc8a1f3ae5a967ad279fd23a870db4afef74a
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-11-24 12:52:50 +0700

    header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <h1 className="text-3xl font-bold">Hello</h1>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="text-center">
+      <h1 className="text-header-text mb-5 text-3xl font-medium">
+        Assembly: Endgame
+      </h1>
+      <p className="text-header-paragraph max-w-[450px] text-lg/snug">
+        Guess the word within 8 attempts to keep the programming world safe from
+        Assembly!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
