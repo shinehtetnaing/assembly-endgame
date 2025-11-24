1bccdd8624991a035577127f6e6535bf0f843c78
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-11-24 13:04:44 +0700

    status section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import Status from "./components/Status";
 
 function App() {
   return (
-    <main>
+    <main className="space-y-8">
       <Header />
+
+      <Status />
     </main>
   );
 }
---
+++ src/components/Status.tsx
@@ -0,0 +1,10 @@
+const Status = () => {
+  return (
+    <section className="bg-green-bg text-status-text flex flex-col items-center justify-center gap-5 rounded py-3">
+      <h2 className="text-xl font-semibold">You win!</h2>
+      <p className="italic">Well done! 🎉</p>
+    </section>
+  );
+};
+
+export default Status;
