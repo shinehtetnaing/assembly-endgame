e7efdcebae0b8ba7a1198723b751e29cea8380a1
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-11-25 10:01:44 +0700

    word section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import LanguageChips from "./components/LanguageChips";
 import Status from "./components/Status";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState<string>("react");
+
   return (
     <main className="max-w-[450px] space-y-8">
       <Header />
@@ -10,6 +13,17 @@ function App() {
       <Status />
 
       <LanguageChips />
+
+      <section className="flex items-center justify-center">
+        {currentWord.split("").map((letter, index) => (
+          <span
+            key={index}
+            className="bg-word-bg ml-3 flex size-16 items-center justify-center border-b-2 text-3xl capitalize"
+          >
+            {letter}
+          </span>
+        ))}
+      </section>
     </main>
   );
 }
